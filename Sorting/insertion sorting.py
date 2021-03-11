6df3f448122b7a0761524ab4468a70fb146f6557
pome95 <pome95@kakao.com>
2021-03-11 20:30:09 +0900

    삽입 정렬 기초

--- Sorting/insertion sorting.py
+++ Sorting/insertion sorting.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import random
 
 def insertion_sort(data):
@@ -15,11 +12,11 @@ def insertion_sort(data):
                 break
     return data
 
-
-# In[3]:
-
-
 data_list = random.sample(range(100),20)
 print(data_list)
 print(insertion_sort(data_list))
 
+# Result => 
+# [49, 24, 12, 72, 8, 17, 37, 62, 7, 71, 82, 79, 13, 61, 90, 23, 31, 99, 19, 6]
+#
+# [6, 7, 8, 12, 13, 17, 19, 23, 24, 31, 37, 49, 61, 62, 71, 72, 79, 82, 90, 99]
